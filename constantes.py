0b0b9162eaded6e9cd17c7c7276730a310100e26
nikollas <nikollasantes@gmail.com>
2023-05-02 20:17:51 -0300

    criando o main

---
+++ constantes.py
@@ -0,0 +1,13 @@
+# dimensões da tela
+LARGURA = 1080
+ALTURA = 720
+
+# título do jogo
+TITULO_JOGO = 'Pokemon Tower Defense'
+
+#FPS
+FPS = 60
+
+#cores
+PRETO = (0, 0, 0)
+BRANCO = (255, 255, 255)

90249cdf0cb74dd6da159a161e66ac816af60f61
nikollas <nikollasantes@gmail.com>
2023-05-04 23:18:34 -0300

    tela inicial básica

--- constantes.py
+++ constantes.py
@@ -5,9 +5,19 @@ ALTURA = 720
 # título do jogo
 TITULO_JOGO = 'Pokemon Tower Defense'
 
-#FPS
+# FPS
 FPS = 60
 
-#cores
-PRETO = (0, 0, 0)
+# cores
+VERMELHO = (255, 0, 0)
+VERDE = (0, 255, 0)
+AZUL = (0, 0, 255)
 BRANCO = (255, 255, 255)
+PRETO = (0, 0, 0)
+
+# spritesheets
+KANTO_PARADO = '01_kanto_parado.png'
+KANTO_MOVIMENTO = '01_kanto_movimento.png'
+
+# fonte
+FONTE = 'arial'
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 import constantes
 import sprites
+import os
 
 class Game:
     def __init__(self):
@@ -11,6 +12,8 @@ class Game:
         pygame.display.set_caption(constantes.TITULO_JOGO)
         self.relogio = pygame.time.Clock()
         self.esta_rodando = True
+        self.fonte = pygame.font.match_font(constantes.FONTE)
+        self.carregar_arquivos()
 
     def novo_jogo(self):
         # instancia as classes das sprites do jogo
@@ -44,14 +47,71 @@ class Game:
         self.todas_as_sprites.draw(self.tela)
         pygame.display.flip()
 
+    def carregar_arquivos(self):
+        # carrega os arquivos de audio e de imagem
+        self.diretorio_bg = os.path.join(os.getcwd(), 'img', 'bg')
+        self.diretorio_sprites = os.path.join(os.getcwd(), 'img', 'pokemon_parado')
+        self.diretorio_movimento = os.path.join(os.getcwd(), 'img', 'pokemon_movimento')
+        self.diretorio_mapas = os.path.join(os.getcwd(), 'img', 'mapas')
+        self.diretorio_cries = os.path.join(os.getcwd(), 'som', 'cries')
+        self.diretorio_efeitos = os.path.join(os.getcwd(), 'som', 'efeitos_gerais')
+        self.diretorio_movesound = os.path.join(os.getcwd(), 'som', 'moves')
+        self.diretorio_soundtrack = os.path.join(os.getcwd(), 'som', 'soundtrack')
+        self.kanto_spritesheet = os.path.join(self.diretorio_sprites, constantes.KANTO_PARADO)
+        self.kanto_movimento = os.path.join(self.diretorio_sprites, constantes.KANTO_MOVIMENTO)
+
+
+    def mostrar_texto(self, texto, tamanho, cor, x, y):
+        # exibe um texto na tela
+        fonte = pygame.font.Font(self.fonte, tamanho)
+        texto = fonte.render(texto, False, cor)
+        texto_rect = texto.get_rect()
+        texto_rect.midtop = (x, y)
+        self.tela.blit(texto, texto_rect)
+
     def mostrar_tela_start(self):
-        pass
+        pygame.mixer.music.load(os.path.join(self.diretorio_soundtrack, '01 - Opening Movie.mp3'))
+        pygame.mixer.music.play()
+        self.mostrar_texto(
+            'Pokémon Tower Defense',
+            32,
+            constantes.BRANCO,
+            constantes.LARGURA / 2,
+            constantes.ALTURA * 0.09
+        )
+        self.mostrar_texto(
+            'Desenvolvido por Níkollas Antes',
+            18,
+            constantes.BRANCO,
+            constantes.LARGURA / 2,
+            constantes.ALTURA * 0.9
+        )
+        pygame.display.flip()
+        self.esperar_musica()
+
+    def esperar_musica(self):
+        esperando_musica = True
+        while esperando_musica:
+            self.relogio.tick(constantes.FPS)
+            if not pygame.mixer.music.get_busy():
+                esperando_musica = False
+
+    def esperar_comando(self):
+        esperando_comando = True
+        while esperando_comando:
+            self.relogio.tick(constantes.FPS)
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    esperando_comando = False
+                    self.esta_rodando = False
+                if event.type == pygame.MOUSEBUTTONUP:
+                    esperando_comando = False
 
     def mostrar_tela_game_over(self):
         pass
 
 g = Game()
-g.mostrar_tela_start
+g.mostrar_tela_start()
 
 while g.esta_rodando:
     g.novo_jogo()
